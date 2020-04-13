44b0f16212a6fcbd66f5302bd0f42bc91cf9d63f
jadlerfisher <jadlerfisher@gmail.com>
2020-04-13 11:07:58 -0500

    First Commit

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "30px";
+  } else {
+    document.getElementById("header").style.fontSize = "90px";
+  }
+}
